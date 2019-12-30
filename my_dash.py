6b5c6aae1f4d5af741b3bc9e0829d9aa01e4595f
Yiyuan Ruan <1533724109@qq.com>
2019-12-31 07:21:48 +0800

    add *.npy

--- my_dash.py
+++ my_dash.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import dash_daq as daq
-from flask_caching import Cache
+# from flask_caching import Cache
 import plotly.graph_objs as go
 from datetime import datetime
 import pandas as pd
